11e80fe0e07822c4cf73dd3882a2394086123460
wilsonmar <wilsonmar@gmail.com>
2019-09-01 22:32:53 -0400

    Troubleshooting kedro test

--- kedro/kedro-sample-setup.sh
+++ kedro/kedro-sample-setup.sh
@@ -21,29 +21,24 @@
 
 # CURRENT STATUS: under construction. 
 
-### 1. Collect parameters controlling the run:
+set -e  # stop on error.
 
-INSTALL_UTILITIES="reinstall"  # no or reinstall (yes)
-INSTALL_USING_BREW="no"  # no or yes
-
-BASHFILE="$HOME/.bash_profile"  # on Macs (alt .bashrc)
-WORK_FOLDER="projects"  # as in ~/projects
-WORK_REPO="kedro-sample-setup"  # by default the same name as the script file.
-
-GITHUB_ACCOUNT="quantumblacklabs"
-GITHUB_REPO="kedro"
-CONDA_ENV="Kedro1"
-BINARY_STORE_PATH="/usr/local/bin"
-
-SCRIPT_PATH="src"
-#SCRIPT_PATH="./kedro/fiori/example.ts"
-
-REMOVE_AT_END="no"  # yes or no (during debugging)
-# TODO: Add invocation parameter override handling
-
-
-### 2. Context: Starting time stamp, OS versions, command attributes:
+### 0. Set display utilities:
 clear  # screen and history?
+
+### Set color variables (based on aws_code_deploy.sh): 
+bold="\e[1m"
+dim="\e[2m"
+underline="\e[4m"
+blink="\e[5m"
+reset="\e[0m"
+red="\e[31m"
+green="\e[32m"
+blue="\e[34m"
+
+h2() {
+  printf "\n${bold}>>> %s${reset}\n" "$(echo "$@" | sed '/./,$!d')"
+}
 
 THISPGM="$0"
 TIME_START="$(date -u +%s)"
@@ -63,7 +58,30 @@ FREE_DISKBLOCKS_START="$(df | awk '{print $4}' | cut -d' ' -f 6)"
 printf ">>> INSTALL_UTILITIES=\"%s\" (no or yes or reinstall)\n" "$INSTALL_UTILITIES"
 
 
-### OS detection to PACKAGE_INSTALLER variable:
+h2 "1. Collect parameters controlling run:"
+
+INSTALL_UTILITIES="yes"  # no or reinstall (yes)
+INSTALL_USING_BREW="no"  # no or yes
+
+BINARY_STORE_PATH="/usr/local/bin"
+BASHFILE="$HOME/.bash_profile"  # on Macs (alt .bashrc)
+WORK_FOLDER="projects"  # as in ~/projects
+WORK_REPO="kedro-sample-setup"  # by default the same name as the script file.
+
+CONDA_ENV="Kedro1"
+GITHUB_ACCOUNT="quantumblacklabs"
+GITHUB_BASE_REPO="kedro"
+GITHUB_SCRIPT_REPO="kedro-examples"
+TEST_PATH="$HOME/projects/kedro-sample-setup/kedro-examples/kedro-tutorial/"
+SCRIPT_PATH="$HOME/projects/kedro-sample-setup/kedro-examples/kedro-tutorial/src/kedro_tutorial"
+
+#SCRIPT_PATH="./kedro/fiori/example.ts"
+
+REMOVE_AT_END="no"  # yes or no (during debugging)
+# TODO: Add invocation parameter override handling
+
+
+h2 "2. OS detection to set PACKAGE_INSTALLER variable:"
 PACKAGE_INSTALLER='unknown'  # initial value.
 unamestr=$( uname )  # get from os.
 if [ "$unamestr" = 'Darwin' ]; then
@@ -75,22 +93,31 @@ elif [ "$unamestr" = 'Linux' ]; then
        PACKAGE_INSTALLER="yum"      # use yum update
    elif [ -f /etc/lsb-release ]; then  # ubuntu 
        PACKAGE_INSTALLER="apt-get"  # use apt-get update
+   else
+      printf ">>> What installer?  Exiting..."
+      exit
    fi
 elif [ "$unamestr" = 'FreeBSD' ]; then
     PACKAGE_INSTALLER='pkg'  # https://www.freebsd.org/cgi/man.cgi?query=pkg-install
 elif [ "$unamestr" = 'Windows' ]; then
     PACKAGE_INSTALLER='choco'
 fi
+
+if [ "$PACKAGE_INSTALLER" = 'brew' ]; then
+   printf ">>> MacOS %s \n" "$( system_profiler SPSoftwareDataType | grep "Version" )"
+elif [ "$unamestr" = 'Windows' ]; then
+   printf ">>> WindowsOS = \"%s\" \n"  "$( uname -a )"
+else
+   printf ">>> Linux OS = \"%s\" \n"  "$( uname -a )"
+fi
+
 if [ $PACKAGE_INSTALLER != 'brew' ]; then
    printf ">>> This script is not designed for %s  = %s.\n" "$unamestr" "$PACKAGE_INSTALLER"
    exit
-else
-   printf ">>> PACKAGE_INSTALLER = \"%s\" \n"  "$PACKAGE_INSTALLER"
-   # uname -a  # operating sytem
 fi
 
 
-### 3. Shell utility functions:
+h2 "3. Shell utility functions:"
 
 # TODO: Set TRAP.
 cleanup() {
@@ -117,39 +144,11 @@ cleanup() {
    exit $err 
 }
 
-# Define script utility functions:
+
+h2 "4. Pre-requisites installation utilities:"
 command_exists() {  # in /usr/local/bin/... if installed by brew
   command -v "$@" > /dev/null 2>&1
 }
-
-
-### 4. Delete local repository if it's there (for idempotency):
-
-cd "$HOME" || exit
-   if [ ! -d "$WORK_FOLDER" ]; then # NOT found:
-      printf "\n>>> Creating folder path %s to hold folder created/deleted by this script... " "$HOME/$WORK_FOLDER"
-       mkdir "$WORK_FOLDER"
-   fi
-          cd "$WORK_FOLDER" || exit
-
-   if [   -d "$WORK_REPO" ]; then # found:
-      printf "\n>>> Removing ~/%s (from previous run)...\n" "$WORK_FOLDER/$WORK_REPO"
-      rm -rf "$WORK_REPO"
-   fi
-   if [  -d "$WORK_REPO" ]; then # NOT found:
-      printf "\n>>> Folder ~/%s delete by script logic failed.\n" "$WORK_FOLDER/$WORK_REPO"
-      exit
-   else
-      printf ">>> Creating folder ~/%s ...\n" "$WORK_FOLDER/$WORK_REPO"
-      mkdir "$WORK_REPO"
-   fi
-         cd "$WORK_REPO" || exit
-
-   printf "PWD=%s \n" "$PWD"
-   # ls -al
-
-
-### 5. Pre-requisites installation utilities:
 
 if ! command_exists python3 ; then  # not exist:
    if [ "$INSTALL_UTILITIES" = "no" ]; then
@@ -180,11 +179,9 @@ if [ "$PACKAGE_INSTALLER" = "brew" ]; then
    fi
 
    if ! command_exists conda ; then  # not exist:
-      if [ "$INSTALL_UTILITIES" = "no" ]; then
-         printf "\n>>> Conda not installed but INSTALL_UTILITIES=\"no\". Exiting...\n"
-         exit
-      else
+      printf "\n>>> Installing anaconda (v3)...\n"
          brew cask install anaconda
+
          # if successful:
          # Initialize Conda to the Linux shell being used:
          conda init bash
@@ -196,15 +193,15 @@ if [ "$PACKAGE_INSTALLER" = "brew" ]; then
             printf "\n>>> Adding export $CONDA_PATH in $BASHFILE \n"
             # GOHOME="$HOME/golang1"  # where you store custom go source code
             # export GOHOME="$HOME/gits/wilsonmar/golang-samples"
+            source "$BASHFILE"  # to activate changes (on one session)
          fi
-      fi
    fi
    printf "\n>>> Conda version installed ==> %s \n" "$( conda --version)"
 
 fi  # brew
 
 
-### 6. install Kedro cli
+h2 "5. Create Kedro folder:"
 
 github_latest_release() {  # generic function:
    # Based on https://gist.github.com/lukechilds/a83e1d7127b78fef38c2914c4ececc3c
@@ -214,97 +211,118 @@ github_latest_release() {  # generic function:
    # Alternative using jq utility if installed by brew:
    curl --silent "https://api.github.com/repos/$1/releases/latest" | jq -r .tag_name
 }
-   KEDRO_LATEST_RELEASE=$( github_latest_release "$GITHUB_ACCOUNT/$GITHUB_REPO" )
-   printf "\n>>> %s latest release in GitHub ==> %s \n" "$GITHUB_ACCOUNT/$GITHUB_REPO" \
+   KEDRO_LATEST_RELEASE=$( github_latest_release "$GITHUB_ACCOUNT/$GITHUB_BASE_REPO" )
+   printf "\n>>> %s latest release in GitHub ==> %s \n" "$GITHUB_ACCOUNT/$GITHUB_BASE_REPO" \
       "$KEDRO_LATEST_RELEASE"
    # Example Response: 0.15.0
 
-exit
 
+h2 "6. install Kedro cli:"
 
 # Based on https://github.com/quantumblacklabs/kedro
 install_kedro_cli(){
-      printf "\n>>> Obtaining kedro-cli ...\n" 
       if [ "$PACKAGE_INSTALLER" = "brew" ]; then 
+         printf "\n>>> Installing kedro using pip3 ...\n" 
          # NOTE: Here "pip3" is used instead of "pip" as shown in Kendro's documentation.
          pip3 install kedro==$KEDRO_LATEST_RELEASE
       else  # "linux"
-         printf "\n>>> At this point, this script has only been written/tested on MacOS.\n" 
+         printf "\n>>> At this point, this script has only been written/tested on MacOS.\n"
+         exit
       fi
 }
 if ! command_exists kedro ; then  # not exist:
    if [ "$INSTALL_UTILITIES" = "no" ]; then
       printf "\n>>> kedro-cli not installed but INSTALL_UTILITIES=\"no\". Exiting...\n"
       exit
+   else
+      install_kedro_cli
    fi
-      if [ "$PACKAGE_INSTALLER" = "brew" ]; then
-         if [ "$INSTALL_USING_BREW" = "yes" ]; then
-            printf "\n>>> Brew install kedro-io/taps/kedro ...\n"
-            brew install kedro-io/taps/kedro
-                #   ==> Downloading https://registry.npmjs.org/@kedro/kedro-cli/-/kedro-cli-1.0.
-                # 
-         else
-            install_kedro_cli
-         fi 
-      else  # linux
-         printf "\n>>> Installing kedro-cli ...\n"
-         install_kedro_cli
-      fi
 else
    if [ "$INSTALL_UTILITIES" = "reinstall" ]; then
-      printf "\n>>> Reinstalling kedro-cli ...\n"
-      if [ "$PACKAGE_INSTALLER" = "brew" ]; then
-         if [ "$INSTALL_USING_BREW" = "yes" ]; then
-            brew upgrade kedro
-               # Error: kedro-cli 1.14.1 already installed
-         else
-            install_kedro_cli
-         fi 
-      else  # linux
-         install_kedro_cli
-      fi
+      printf "\n>>> -Upgrading kedro using pip3 ...\n"
+      pip3 install -U kedro==$KEDRO_LATEST_RELEASE
    else
-      printf ">>> Using existing version \n" 
+      printf ">>> Using existing kedro version \n" 
    fi
 fi
-printf "\n>>> Kedro-cli %s ...\n" "$( kedro --version "v" )"
-# kedro -h
+printf ">>> Kedro version ==> %s ...\n" "$( kedro --version )"
+   # kedro, version 0.15.0
+# kedro -h  # for help
 
-exit
+which kedro
+
+#   printf "\n>>> pop-up docs webpage in default browser:\n"
+#   kedro docs
 
 
-### 7. Clone script from GitHub
+h2 "7. TODO: Enter Conda environment:"
 
-# Because its parent folder is deleted before/after use:
 
-         if [ ! -d "$GITHUB_ACCOUNT/$GITHUB_REPO" ]; then  # folder not there
-            fancy_echo "Creating folder $GITHUB_ACCOUNT/$GITHUB_REPO ..."
+
+h2 "8. Delete local repository if it's there (for idempotency):"
+
+cd "$HOME" || exit
+   if [ ! -d "$WORK_FOLDER" ]; then # NOT found:
+      printf "\n>>> Creating folder path %s to hold folder created/deleted by this script... " "$HOME/$WORK_FOLDER"
+       mkdir "$WORK_FOLDER"
+   fi
+          cd "$WORK_FOLDER" || exit
+
+   if [   -d "$WORK_REPO" ]; then # found:
+      printf "\n>>> Removing ~/%s (from previous run)...\n" "$WORK_FOLDER/$WORK_REPO"
+      rm -rf "$WORK_REPO"
+   fi
+   if [  -d "$WORK_REPO" ]; then # NOT found:
+      printf "\n>>> Folder ~/%s delete by script logic failed.\n" "$WORK_FOLDER/$WORK_REPO"
+      exit
+   else
+      printf ">>> Creating folder ~/%s ...\n" "$WORK_FOLDER/$WORK_REPO"
+      mkdir "$WORK_REPO"
+   fi
+         cd "$WORK_REPO" || exit
+
+   printf "PWD=%s \n" "$PWD"
+   # ls -al
+
+
+h2 "9. Download sample project:"
+
+         # Because its parent folder is deleted before/after use:
+         if [ ! -d "$GITHUB_ACCOUNT/$GITHUB_SCRIPT_REPO" ]; then  # folder not there
+            printf "\n>>> Creating folder %s ...\n" "$GITHUB_ACCOUNT/$GITHUB_SCRIPT_REPO"
             # option: populate by git clone https://github.com/wilsonmar/golang-samples"
-            git clone https://github.com/$GITHUB_ACCOUNT/$GITHUB_REPO  --depth=1 
+            git clone https://github.com/$GITHUB_ACCOUNT/$GITHUB_SCRIPT_REPO  --depth=1 
                # --depth=1 used to reduce disk space and download time if only latest master is used.
                # RESPONSE: Receiving objects: 100% (1075/1075), 2.17 MiB | 1.43 MiB/s, done.
-            cd $GITHUB_REPO
+            cd $GITHUB_SCRIPT_REPO
          fi
 
-SCRIPT_PATH="04-Challenging_DOM.ts"
-#SCRIPT_PATH="./kedro/fiori/example.ts"
+h2 "10. kedro test (expand screen):"
+pushd "$TEST_PATH" >/dev/null
+   printf "\n>>>  kedro test in %s ...\n" "$TEST_PATH"
+      # By default: SCRIPT_PATH="$HOME/projects/kedro-sample-setup/kedro-examples/kedro-tutorial/src/kedro_tutorial"
+   kedro test
+popd >/dev/null
+pwd
 
 
-### 8. Run the script
+h2 "11. Run the script:"
 
-if [ REMOVE_AT_END = "no" ]; then 
-   printf "\n>>> npx running script %s \n" "$SCRIPT_PATH"
-   npx @kedro/kedro-cli run "$SCRIPT_PATH" --no-headless
-else
-   printf "\n>>> kedro running script %s \n" "$SCRIPT_PATH"
-   kedro run "$SCRIPT_PATH" --no-headless
-fi
+pushd "$SCRIPT_PATH" >/dev/null
+   printf "\n>>> kedro run script from %s \n" "$SCRIPT_PATH"
+   kedro run
+   # In kedro_tutorial/io: xls_local.py
+   # In kedro_tutorial/nodes: pipeline.py run.py
+popd >/dev/null
 
-### 9. Clean up
+
+h2 "12. Exit Conda env and clean up:"
 
 if [ "$REMOVE_AT_END" = "yes" ]; then
    printf "\n>>> Removing WORK_REPO=%s in %s \n" "$WORK_REPO" "$PWD"
    rm -rf "$WORK_REPO"
+else
+   printf "\n>>> REMOVE_AT_END=\"%s\", files remain in %s \n" "$REMOVE_AT_END" "$PWD"
 fi
 
 # per https://kubedex.com/troubleshooting-aws-iam-authenticator/
