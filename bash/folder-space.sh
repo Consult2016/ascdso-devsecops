db9273534063846e1df2c0db9c32a360a7b950cd
Wilson Mar <wilsonmar+github@gmail.com>
2019-10-18 14:19:51 -0400

    Add folder-space.sh in bash

---
+++ bash/folder-space.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# This reports on the diskspace specified in the first argument, as in:
+# USAGE: ./folder-space.sh ~/var
+
+# This is https://github.com/wilsonmar/DevSecOps/blob/master/bash/folder-space.sh
+# Adapted from https://unix.stackexchange.com/questions/53841/how-to-use-a-timer-in-bash
+# bash -c "$(curl -fsSL https://raw.githubusercontent.com/wilsonmar/DevSecOps/master/bash/folder-space.sh)"
+
+# This is free software; see the source for copying conditions. There is NO
+# warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+# Capture starting timestamp:
+   start=$(date +%s)
+
+# Example: sleep $1
+
+   # du -csh /data/registry/docker/registry
+   echo "$ du -csh $1"
+           du -csh $1
+
+# Capture endining timestamp:
+   end=$(date +%s)
+
+# Calculate difference:
+   seconds=$(echo "$end - $start" | bc )
+   #echo $seconds' sec'
+   echo 'Elapsed: ' $( awk -v t=$seconds 'BEGIN{t=int(t*1000); printf "%d:%02d:%02d\n", t/3600000, t/60000%60, t/1000%60}' )
