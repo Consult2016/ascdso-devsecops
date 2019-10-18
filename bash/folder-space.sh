031236e6cce7a7e995b652e81024ab453d3481c5
Wilson Mar <wilsonmar+github@gmail.com>
2019-10-18 14:56:40 -0400

    Change -csh to -cs for precise vs human-readable

--- bash/folder-space.sh
+++ bash/folder-space.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 # This reports on the diskspace specified in the first argument, as in:
-# USAGE: ./folder-space.sh ~/var
+# USAGE: 
+#   curl -O https://raw.githubusercontent.com/wilsonmar/DevSecOps/master/bash/folder-space.sh
+#   chmod +x folder.space.sh
+#   ./folder-space.sh ~/var/log
+# RESPONSE:
+#   38M   /var/log
 
 # This is https://github.com/wilsonmar/DevSecOps/blob/master/bash/folder-space.sh
 # Adapted from https://unix.stackexchange.com/questions/53841/how-to-use-a-timer-in-bash
-# bash -c "$(curl -fsSL https://raw.githubusercontent.com/wilsonmar/DevSecOps/master/bash/folder-space.sh ~/var)"
+# bash -c "$(curl -fsSL https://raw.githubusercontent.com/wilsonmar/DevSecOps/master/bash/folder-space.sh)"
 
 # This is free software; see the source for copying conditions. There is NO
 # warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
@@ -16,8 +21,9 @@
 # Example: sleep $1
 
    # du -csh /data/registry/docker/registry
-   echo "$ du -csh $1"
-           du -csh $1
+   echo "$ du -cs $1"
+           du -cs $1
+#           du -csh $1
 
 # Capture endining timestamp:
    end=$(date +%s)
@@ -26,3 +32,4 @@
    seconds=$(echo "$end - $start" | bc )
    #echo $seconds' sec'
    echo 'Elapsed: ' $( awk -v t=$seconds 'BEGIN{t=int(t*1000); printf "%d:%02d:%02d\n", t/3600000, t/60000%60, t/1000%60}' )
+   
