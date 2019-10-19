8924617b75e3ec259c18d601980cea72428cf08c
wilsonmar <wilsonmar@gmail.com>
2019-10-19 07:22:55 -0400

    update timer-demo.sh

--- bash/timer-demo.sh
+++ bash/timer-demo.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
 
-# ./timer-demo.sh in https://github.com/wilsonmar/DevSecOps/blob/master/bash/timer-demo.sh
+# This reports on the diskspace specified in the first argument, as in:
+# USAGE: 
+#   curl -O https://raw.githubusercontent.com/wilsonmar/DevSecOps/master/bash/folder-space.sh
+#   chmod +x folder.space.sh
+#   ./folder-space.sh ~/var/log
+# RESPONSE:
+#   38M   /var/log
+
+# This is https://github.com/wilsonmar/DevSecOps/blob/master/bash/folder-space.sh
 # Adapted from https://unix.stackexchange.com/questions/53841/how-to-use-a-timer-in-bash
-# USAGE:
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/wilsonmar/DevSecOps/master/bash/timer-demo.sh)"
+# bash -c "$(curl -fsSL https://raw.githubusercontent.com/wilsonmar/DevSecOps/master/bash/folder-space.sh)"
 
 # This is free software; see the source for copying conditions. There is NO
 # warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
-start=$(date +%s)
-#
-# do something
-sleep $1
-#
-#
-end=$(date +%s)
+# Capture starting timestamp:
+   start=$(date +%s)
 
-seconds=$(echo "$end - $start" | bc)
-echo $seconds' sec'
+# Example: sleep $1
 
-echo 'Elapsed: '
-awk -v t=$seconds 'BEGIN{t=int(t*1000); printf "%d:%02d:%02d\n", t/3600000, t/60000%60, t/1000%60}'
+   # du -csh /data/registry/docker/registry
+   echo "$ du -cs $1"
+           du -cs $1
+#           du -csh $1
+
+# Capture endining timestamp:
+   end=$(date +%s)
+
+# Calculate difference:
+   seconds=$(echo "$end - $start" | bc )
+   #echo $seconds' sec'
+   echo 'Elapsed: ' $( awk -v t=$seconds 'BEGIN{t=int(t*1000); printf "%d:%02d:%02d\n", t/3600000, t/60000%60, t/1000%60}' )
